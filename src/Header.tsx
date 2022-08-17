b855038efa9e4d8f8f5f94477d6298a9e3bdb757
LySkyn <alexej.pisarchuk@gmail.com>
2022-08-17 17:41:16 +0200

    1

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            123
+        </div>
+    )
+};
+export default Header
---
+++ src/Technologies.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+        <div>
+            456
+        </div>
+    );
+};
+export default Technologies;
