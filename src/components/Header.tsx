59e261546064739027376e6fcef56a8a3a64d49f
LySkyn <alexej.pisarchuk@gmail.com>
2022-08-21 17:21:07 +0200

    Samurai 12

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://seeklogo.com/images/C/company-name-logo-09881CAD1A-seeklogo.com.png'/>
+        </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://cdn.thecrazytourist.com/wp-content/uploads/2019/02/ccimage-shutterstock_519559345.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
