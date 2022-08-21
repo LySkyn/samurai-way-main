9d5e8272b73326ed88e44b5d27f13a2360c8846d
LySkyn <alexej.pisarchuk@gmail.com>
2022-08-21 17:20:53 +0200

    Samurai 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 function App() {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src='https://seeklogo.com/images/C/company-name-logo-09881CAD1A-seeklogo.com.png'/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img
-                        src='https://cdn.thecrazytourist.com/wp-content/uploads/2019/02/ccimage-shutterstock_519559345.jpg'/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New Post
-                    </div>
-                    <div>
-                        <div>
-                            post 1
-                        </div>
-                        <div>
-                            post 2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
-    );
+    )
+        ;
 }
 
 
