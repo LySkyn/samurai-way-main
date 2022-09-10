b340d96eb6158e5b60ddc347bb8a9b53bbd88140
LySkyn <alexej.pisarchuk@gmail.com>
2022-09-10 21:30:31 +0200

    Samurai 19

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import s from './Post.module.css'
 
-export const Post = (props) => {
+type PostType = {
+    message: string
+}
+export const Post = (props: PostType) => {
     return (
         <div className={s.item}>
             <img src={'https://static.wikia.nocookie.net/mrbean/images/4/4b/Mr_beans_holiday_ver2.jpg'}/>
