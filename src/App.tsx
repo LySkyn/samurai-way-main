8b524cc9bc06ae1280b60c0f00ca9f0a07e509c3
LySkyn <alexej.pisarchuk@gmail.com>
2022-09-15 22:25:02 +0200

    Samurai 27

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,10 @@ function App() {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
+                    {/*<Route path='/dialogs' component={Dialogs}/>*/}
+                    {/*<Route path='/profile' component={Profile}/>*/}
+                    <Route path='/dialogs' render={() => <Dialogs/>}/>
+                    <Route path='/profile' render={() => <Profile/>}/>
                     <Route path='/news' render={() => "News"}/>
                     <Route path='/music' render={() => "Music"}/>
                     <Route path='/settings' render={() => "Settings"}/>
