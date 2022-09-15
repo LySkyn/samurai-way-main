74643a367cabe76d9ce1322759ea070c4f98765d
LySkyn <alexej.pisarchuk@gmail.com>
2022-09-15 22:00:06 +0200

    Samurai 26

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
-import {message} from "antd";
-
-const DialogItem = (props: any) => {
-    let path = "/dialogs/" + props.id
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
-
-const Message = (props: any) => {
-    return <div className={s.message}>{props.message}</div>
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 let dialogsData = [
     {id: 1, name: "Dimych"},
